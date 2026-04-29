c6e20556ac329fc00db61b44ac8de5bb0b11ab7b
uinklashes-ship-it <uinklashes@gmail.com>
2026-04-29 16:17:56 -0500

    Create main.jsx

---
+++ src/src/main.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import ShariftChicaPage from "./ShariftChicaPage.jsx";
+
+createRoot(document.getElementById("root")).render(<ShariftChicaPage />);
